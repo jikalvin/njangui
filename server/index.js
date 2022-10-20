c947619120be6dd08a80a43e27fc8e8867e95384
unknown <pamelafugua@gmail.com>
2022-10-20 21:24:24 +0200

    working on server

--- server/index.js
+++ server/index.js
@@ -18,3 +18,47 @@ const typeDefs = `
     messages: [Message!]
   }
 `;
+
+const messages = [];    //holds all messages sent
+const subscribers = [];   //stores any new messages upon listening
+
+//pushing new users to the subscribers array
+const onMessagesUpdates = (fn) => subscribers.push(fn);
+
+const resolvers = {
+  Query: {
+    messages: () => messages,
+  },
+
+ Mutation: {
+  postMessage: (parent, { user, text }) => {
+    const id = messages.length; //create the id for new message
+    messages.push({id, user, text}); //push Message object to messages array
+    subscribers.forEach((fn) => fn());
+    return id; //return the id
+    },
+  },
+  Subscription: {
+    messages: {
+      subscribe: (parent, args, { pubsub }) => {
+        //create random number as the channel to publish messages to
+        const channel = Math.random().toString(36).slice(2, 15);
+
+        //push the user to the subscriber array with onMessagesUpdates function and 
+        //publish updated messages array to the channel as the callback
+        onMessagesUpdates(() => pubsub.publish(channel, { messages }));
+
+        //publish all messages immediately once a user subscribed
+        setTimeout(() => pubsub.publish(channel, { messages }), 0);
+
+        //returns the asyncIterator
+        return pubsub.asyncIterator(channel);
+      },
+    },
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
+server.start(({ port }) => {
+  console.log(`Server on http://localhost:${port}/`);
+});
